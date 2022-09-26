9d8867f73a3db40eb9bb35a9751e38d8f71bfff4
Matthew Domi <70774927+matthewdomi@users.noreply.github.com>
2022-09-26 16:36:56 +0000

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>final pitch</title>
+        <meta name="meaning of words" content="Dictionary app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      
+    </div>
+  )
+}
